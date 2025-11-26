046401c3fd72cdd98602f0e667db1484e48616a0
Devyansh Roy <devyanshroy24@gmail.com>
2025-11-27 03:06:51 +0530

    nested linkedlist

--- myDic.cpp
+++ myDic.cpp
@@ -1,14 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node 
+struct WordNode 
 {
     string word;
     string meaning;
-    Node* next;
+    WordNode* next;
 };
 
-Node* head = NULL;
+struct FileNode
+{
+    string filename;
+    WordNode* words;  
+    FileNode* next;
+};
+
+FileNode* fileHead = nullptr;
+
 string getFile(char ch) 
 {
     ch = tolower(ch);
@@ -16,6 +24,51 @@ string getFile(char ch)
     file += ch;     
     file += ".txt"; 
     return file;
+}
+
+void createFileList()
+{
+    FileNode* prev = nullptr;
+    for(char c = 'a'; c <= 'z'; c++)
+    {
+        FileNode* newFile = new FileNode();
+        newFile->filename = getFile(c);
+        newFile->words = nullptr;
+        newFile->next = nullptr;
+
+        if(fileHead == nullptr) fileHead = newFile;
+        else prev->next = newFile;
+
+        prev = newFile;
+    }
+}
+
+void loadFiles() 
+{
+    FileNode* f = fileHead;
+    while(f != nullptr)
+    {
+        ifstream in(f->filename);
+        if(!in) { f = f->next; continue; }
+
+        string line;
+        WordNode* headWords = nullptr;
+        while(getline(in, line))
+        {
+            int pos = line.find(" - ");
+            if(pos == -1) continue;
+            string w = line.substr(0, pos);
+            string m = line.substr(pos + 3); 
+            WordNode* newNode = new WordNode();
+            newNode->word = w;
+            newNode->meaning = m;
+            newNode->next = headWords;
+            headWords = newNode;
+        }
+        f->words = headWords;
+        in.close();
+        f = f->next;
+    }
 }
 
 void addWord() 
@@ -26,40 +79,23 @@ void addWord()
     cout << "Enter meaning: ";
     cin.ignore();
     getline(cin, m);
-    Node* newNode = new Node();
+    string file = getFile(w[0]);
+    FileNode* f = fileHead;
+    while(f != nullptr)
+    {
+        if(f->filename == file)
+            break;
+        f = f->next;
+    }
+    WordNode* newNode = new WordNode();
     newNode->word = w;
     newNode->meaning = m;
-    newNode->next = head;
-    head = newNode;
-    string file = getFile(w[0]);
+    newNode->next = f->words;
+    f->words = newNode;
     ofstream out(file, ios::app);
     out << w << " - " << m << endl;
     out.close();
-    cout << "Word added successfully!"<<endl;
-}
-
-void loadFiles() 
-{
-    for(char c = 'a'; c <= 'z'; c++) 
-    {
-        string file = getFile(c);
-        ifstream in(file);
-        if (!in) continue;
-        string line;
-        while (getline(in, line)) 
-        {
-            int pos = line.find(" - ");
-            if (pos == -1) continue;
-            string w = line.substr(0, pos);
-            string m = line.substr(pos + 1);
-            Node* newNode = new Node();
-            newNode->word = w;
-            newNode->meaning = m;
-            newNode->next = head;
-            head = newNode;
-        }
-        in.close();
-    }
+    cout << "Word added successfully!" << endl;
 }
 
 void searchWord() 
@@ -67,34 +103,48 @@ void searchWord()
     string w;
     cout << "Enter word to search: ";
     cin >> w;
-    Node* temp = head;
-    while (temp != NULL) 
+    FileNode* f = fileHead;
+    while(f != nullptr)
     {
-        if (temp->word == w) 
+        WordNode* temp = f->words;
+        while(temp != nullptr)
         {
-            cout << "Meaning: " << temp->meaning << endl;
-            return;
+            if(temp->word == w)
+            {
+                cout << "Meaning: " << temp->meaning << endl;
+                return;
+            }
+            temp = temp->next;
         }
-        temp = temp->next;
+        f = f->next;
     }
 
-    cout << "Word not found!"<<endl;
+    cout << "Word not found!" << endl;
 }
 
+// Delete a word
 void deleteWord() 
 {
     string w;
     cout << "Enter word to delete: ";
     cin >> w;
-    Node* temp = head;
-    Node* prev = NULL;
-    bool found = false;
-    while (temp != NULL) 
+    string file = getFile(w[0]);
+    FileNode* f = fileHead;
+    while(f != nullptr)
     {
-        if (temp->word == w) 
+        if(f->filename == file) break;
+        f = f->next;
+    }
+    if(!f) return;
+    WordNode* temp = f->words;
+    WordNode* prev = nullptr;
+    bool found = false;
+    while(temp != nullptr)
+    {
+        if(temp->word == w)
         {
             found = true;
-            if (prev == NULL) head = temp->next;
+            if(prev == nullptr) f->words = temp->next;
             else prev->next = temp->next;
             delete temp;
             break;
@@ -102,30 +152,26 @@ void deleteWord()
         prev = temp;
         temp = temp->next;
     }
-    if (!found) 
+    if(!found)
     {
-        cout << "Word not found!"<<endl;
+        cout << "Word not found!" << endl;
         return;
     }
-    string file = getFile(w[0]);
     ifstream in(file);
     ofstream tempFile("temp.txt");
     string line;
-    while (getline(in, line)) 
+    while(getline(in, line))
     {
         int pos = line.find(" - ");
         string wordInFile = line.substr(0, pos);
-
-        if (wordInFile != w) 
-        {
-            tempFile << line << endl;
-        }
+        if(wordInFile != w) tempFile << line << endl;
     }
     in.close();
     tempFile.close();
     remove(file.c_str());
     rename("temp.txt", file.c_str());
-    cout << "Word deleted!"<<endl;
+
+    cout << "Word deleted!" << endl;
 }
 
 void showWordsByLetter()
@@ -133,74 +179,73 @@ void showWordsByLetter()
     char c;
     cout << "Enter starting letter: ";
     cin >> c;
-    Node* temp = head;
-    bool any = false;
-    while (temp != NULL) 
+    string file = getFile(c);
+    FileNode* f = fileHead;
+    while(f != nullptr)
     {
-        if (tolower(temp->word[0]) == tolower(c)) 
-        {
-            cout << temp->word << " - " << temp->meaning << endl;
-            any = true;
-        }
-        temp = temp->next;
+        if(f->filename == file) break;
+        f = f->next;
     }
-    if (!any) cout << "No words found."<<endl;
-}
-
-void showAllWords() 
-{
-    Node* temp = head;
-    if (temp == NULL) 
-    {
-        cout << "No words found."<<endl;
-        return;
-    }
-
-    while (temp != NULL) 
+    if(!f || !f->words) { cout << "No words found." << endl; return; }
+    WordNode* temp = f->words;
+    while(temp != nullptr)
     {
         cout << temp->word << " - " << temp->meaning << endl;
         temp = temp->next;
     }
 }
 
+void showAllWords() 
+{
+    FileNode* f = fileHead;
+    bool any = false;
+    while(f != nullptr)
+    {
+        WordNode* temp = f->words;
+        while(temp != nullptr)
+        {
+            cout << temp->word << " - " << temp->meaning << endl;
+            temp = temp->next;
+            any = true;
+        }
+        f = f->next;
+    }
+    if(!any) cout << "No words found." << endl;
+}
+
 int main() 
 {
+    createFileList();
     loadFiles();
     int choice;
-    while (true) {
-        cout << "+-+-+-+-+DICTIONARY MENU+-+-+-+-+"<<endl;
-        cout << "1. Add a word"<<endl;
-        cout << "2. Search a word"<<endl;
-        cout << "3. Delete a word"<<endl;
-        cout << "4. Show words starting from a letter"<<endl;
-        cout << "5. Show all words"<<endl;
-        cout << "6. Exit"<<endl;
+    while(true)
+    {
+        cout << "+-+-+-+-+DICTIONARY MENU+-+-+-+-+" << endl;
+        cout << "1. Add a word" << endl;
+        cout << "2. Search a word" << endl;
+        cout << "3. Delete a word" << endl;
+        cout << "4. Show words starting from a letter" << endl;
+        cout << "5. Show all words" << endl;
+        cout << "6. Exit" << endl;
         cout << "Enter your choice: ";
         cin >> choice;
-
-        switch (choice) 
+        switch(choice)
         {
-            case 1:
-                addWord();
-                break;
-            case 2:
-                searchWord();
-                break;
-            case 3:
-                deleteWord();
-                break;
-            case 4:
-                showWordsByLetter();
-                break;
-            case 5:
-                showAllWords();
-                break;
-            case 6:
-                cout << "Exiting program..."<<endl;
-                return 0;
-            default:
-                cout << "Invalid choice! Please try again."<<endl;
+            case 1: addWord(); 
+            break;
+            case 2: searchWord(); 
+            break;
+            case 3: deleteWord(); 
+            break;
+            case 4: showWordsByLetter(); 
+            break;
+            case 5: showAllWords(); 
+            break;
+            case 6: cout << "Exiting program..." << endl; 
+            return 0;
+            default: cout << "Invalid choice! Please try again." << endl;
         }
     }
+
     return 0;
 }
